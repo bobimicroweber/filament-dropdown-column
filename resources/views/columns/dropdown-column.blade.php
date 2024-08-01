7b468e1818e32995d1ab5c4ed67f29443923a9a0
Bozhidar <bobi@microweber.com>
2024-08-01 17:30:28 +0300

    Update dropdown-column.blade.php

--- resources/views/columns/dropdown-column.blade.php
+++ resources/views/columns/dropdown-column.blade.php
@@ -19,53 +19,51 @@
 <div
     wire:key="{{ $this->getId() }}.table.record.{{ $recordKey }}.column.{{ $getName() }}.toggle-column.{{ $state ? 'true' : 'false' }}"
 >
-
-
+    
     @if (count($arrayState))
-
 
         <x-filament::dropdown>
             <x-slot name="trigger">
 
 
-        @foreach ($arrayState as $state)
-            @if ($icon = $getIcon($state))
-                @php
-                    $color = $getColor($state) ?? 'gray';
-                    $size = $getSize($state) ?? IconColumnSize::Large;
-                    $iconColor = 'white';
-                @endphp
+                @foreach ($arrayState as $state)
+                    @if ($icon = $getIcon($state))
+                        @php
+                            $color = $getColor($state) ?? 'gray';
+                            $size = $getSize($state) ?? IconColumnSize::Large;
+                            $iconColor = 'white';
+                        @endphp
 
                         <x-filament::button :color="$color">
 
                             <div class="flex gap-2 items-center">
-                <x-filament::icon
-                    :icon="$icon"
-                    @class([
-                        'fi-ta-icon-item',
-                        match ($size) {
-                            IconColumnSize::ExtraSmall, 'xs' => 'fi-ta-icon-item-size-xs h-3 w-3',
-                            IconColumnSize::Small, 'sm' => 'fi-ta-icon-item-size-sm h-4 w-4',
-                            IconColumnSize::Medium, 'md' => 'fi-ta-icon-item-size-md h-5 w-5',
-                            IconColumnSize::Large, 'lg' => 'fi-ta-icon-item-size-lg h-6 w-6',
-                            IconColumnSize::ExtraLarge, 'xl' => 'fi-ta-icon-item-size-xl h-7 w-7',
-                            IconColumnSize::TwoExtraLarge, IconColumnSize::ExtraExtraLarge, '2xl' => 'fi-ta-icon-item-size-2xl h-8 w-8',
-                            default => $size,
-                        },
-                        match ($iconColor) {
-                            'gray' => 'text-gray-400 dark:text-gray-500',
-                            default => 'fi-color-custom text-custom-500 dark:text-custom-400',
-                        },
-                        is_string($iconColor) ? 'fi-color-' . $iconColor : null,
-                    ])
-                    @style([
-                        \Filament\Support\get_color_css_variables(
-                            $iconColor,
-                            shades: [400, 500],
-                            alias: 'tables::columns.icon-column.item',
-                        ) => $iconColor !== 'gray',
-                    ])
-                />
+                                <x-filament::icon
+                                    :icon="$icon"
+                                    @class([
+                                        'fi-ta-icon-item',
+                                        match ($size) {
+                                            IconColumnSize::ExtraSmall, 'xs' => 'fi-ta-icon-item-size-xs h-3 w-3',
+                                            IconColumnSize::Small, 'sm' => 'fi-ta-icon-item-size-sm h-4 w-4',
+                                            IconColumnSize::Medium, 'md' => 'fi-ta-icon-item-size-md h-5 w-5',
+                                            IconColumnSize::Large, 'lg' => 'fi-ta-icon-item-size-lg h-6 w-6',
+                                            IconColumnSize::ExtraLarge, 'xl' => 'fi-ta-icon-item-size-xl h-7 w-7',
+                                            IconColumnSize::TwoExtraLarge, IconColumnSize::ExtraExtraLarge, '2xl' => 'fi-ta-icon-item-size-2xl h-8 w-8',
+                                            default => $size,
+                                        },
+                                        match ($iconColor) {
+                                            'gray' => 'text-gray-400 dark:text-gray-500',
+                                            default => 'fi-color-custom text-custom-500 dark:text-custom-400',
+                                        },
+                                        is_string($iconColor) ? 'fi-color-' . $iconColor : null,
+                                    ])
+                                    @style([
+                                        \Filament\Support\get_color_css_variables(
+                                            $iconColor,
+                                            shades: [400, 500],
+                                            alias: 'tables::columns.icon-column.item',
+                                        ) => $iconColor !== 'gray',
+                                    ])
+                                />
 
                                 @if(isset($options[$stateString]))
                                     {{ $options[$stateString] }}
@@ -73,35 +71,32 @@
                                     Unknown
                                 @endif
 
-                                <x-filament::icon icon="heroicon-o-chevron-down" class="w-5 h-5" />
+                                <x-filament::icon icon="heroicon-o-chevron-down" class="w-5 h-5"/>
 
                             </div>
                         </x-filament::button>
 
+                    @endif
+                @endforeach
 
 
-            @endif
-        @endforeach
+            </x-slot>
 
+            <x-filament::dropdown.list>
+                @foreach($options as $optionKey => $optionValue)
+                    @php
+                        $optionIcon = $getIcon($optionKey);
+                    @endphp
+                    <x-filament::dropdown.list.item>
+                        <div class="flex gap-2">
+                            <x-filament::icon :icon="$optionIcon" class="w-5 h-5"/>
+                            {{ $optionValue }}
+                        </div>
+                    </x-filament::dropdown.list.item>
+                @endforeach
+            </x-filament::dropdown.list>
 
-        </x-slot>
-
-        <x-filament::dropdown.list>
-            @foreach($options as $optionKey => $optionValue)
-                @php
-                $optionIcon = $getIcon($optionKey);
-                @endphp
-                <x-filament::dropdown.list.item>
-                   <div class="flex gap-2">
-                       <x-filament::icon :icon="$optionIcon" class="w-5 h-5" />
-                       {{ $optionValue }}
-                   </div>
-                </x-filament::dropdown.list.item>
-            @endforeach
-        </x-filament::dropdown.list>
-
-    </x-filament::dropdown>
-
+        </x-filament::dropdown>
 
     @elseif (($placeholder = $getPlaceholder()) !== null)
         <x-filament-tables::columns.placeholder>
