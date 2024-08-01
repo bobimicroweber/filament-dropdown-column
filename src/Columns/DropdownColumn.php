8cef6e8d7cfc7520dd99d0f8bb4d83e95829ff47
bobimicroweber <bobimicroweber@users.noreply.github.com>
2024-08-01 14:13:57 +0000

    Fix styling

--- src/Columns/DropdownColumn.php
+++ src/Columns/DropdownColumn.php
@@ -2,23 +2,20 @@
 
 namespace BobiMicroweber\FilamentDropdownColumn\Columns;
 
-use Closure;
 use Filament\Actions\Concerns\HasSize;
 use Filament\Tables\Columns\Column;
-use Filament\Tables\Columns\Concerns\CanBeValidated;
 use Filament\Tables\Columns\Concerns\CanUpdateState;
 use Filament\Tables\Columns\Concerns\HasColor;
 use Filament\Tables\Columns\Concerns\HasIcon;
-use Filament\Tables\Columns\Contracts\Editable;
 use Filament\Tables\Filters\Concerns\HasOptions;
 
 class DropdownColumn extends Column
 {
     use CanUpdateState;
-    use HasIcon;
-    use HasSize;
-    use HasOptions;
     use HasColor;
+    use HasIcon;
+    use HasOptions;
+    use HasSize;
 
     protected string $view = 'filament-dropdown-column::columns.dropdown-column';
 
@@ -29,5 +26,4 @@ class DropdownColumn extends Column
         $this->disabledClick();
 
     }
-
 }
