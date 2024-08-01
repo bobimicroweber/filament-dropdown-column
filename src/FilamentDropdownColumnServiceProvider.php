edd38b1dbb769ae5ce36195bff9e76133f6e17d2
bobimicroweber <bobimicroweber@users.noreply.github.com>
2024-08-01 13:08:53 +0000

    Fix styling

--- src/FilamentDropdownColumnServiceProvider.php
+++ src/FilamentDropdownColumnServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace BobiMicroweber\FilamentDropdownColumn;
 
+use BobiMicroweber\FilamentDropdownColumn\Commands\FilamentDropdownColumnCommand;
+use BobiMicroweber\FilamentDropdownColumn\Testing\TestsFilamentDropdownColumn;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use BobiMicroweber\FilamentDropdownColumn\Commands\FilamentDropdownColumnCommand;
-use BobiMicroweber\FilamentDropdownColumn\Testing\TestsFilamentDropdownColumn;
 
 class FilamentDropdownColumnServiceProvider extends PackageServiceProvider
 {
